b2e1ef63a5d5edc1a6cc78fc72e97b017ebbe338
afkehaya <afkehaya@gmail.com>
2018-05-21 19:48:58 -0700

    made some small edits

--- contract_course.sol
+++ contract_course.sol
@@ -3,9 +3,11 @@ What we're going to build:
 We're building a smart contract that allows the contract creator to deposit money that can only be released when the contract owner sets a boolean value to TRUE.
 
 # First we have to set the pragma line. This lets the compiler know what version we are running.
+
 pragma solidity ^0.4.19;
 
 # Now we have to create the contract. This is similar to a class in other languages.
+
 contract MyContract {
 
 # Now that we have our contract setup, lets create a boolean value so that we can change it later.
@@ -15,15 +17,18 @@ contract MyContract {
    bool public myBool = false;
 
 # Next, let's create an address variable that we can set the contract creators address to.
+
    address public myAddress;
 
 # Just for reference, these are some other variables that are available to us.
 # We have uint8 - uint256 (unsigned integers). You can also have uint16, uint32, etc all the way to 256.. If you don't know what that is I can explain it to you
 # The uint8 in this example is set to public, so it can be read. The uint256 is not public, so it can't be read outside of the contract.
+
    uint8 public myUint8;
    uint256 myUint256;
 
 # You can also create strings and bytes. A string is a something that can be read by a human. Bytes can't.
+
    string myString = "myString";
    bytes myBytes = "myString";
 
@@ -36,6 +41,7 @@ contract MyContract {
    }
 
 # This function allows us to send Ether to the contract. We have to make it both public (so we can see it) and payable so we can send it money.
+
    function fundMyContract() public  payable{
 
    }
@@ -45,11 +51,14 @@ contract MyContract {
 # This means that the myAddress variable must be the account associated with creating the smart contract
 # If the msg.sender (person calling this function) isn't the contract creator, the code in this function won't run so the boolean will stay the statement
 # However, if the msg.sender IS the contract creator, then he/she will be able to change the boolean to true to allow money to be transfered.
-   function setMyBoolean(bool myBoolArgument) public {
-       if(msg.sender == myAddress) {
-        myBool = myBoolArgument;
-       }
-   }
+# In this example, we're using "require" instead of an if statement
+# It checks to make sure that person trying to change the boolean is the contract owner
+# You could also use the word "assert", However, be careful assert will cause any gas spent to be used where as require will just return the gas back to the user.
+
+ function setMyBoolean(bool myBoolArgument) public {
+     require(msg.sender == myAddress);
+     myBool = myBoolArgument;
+  }
 
 # This function allows the second account to withdraw Ether from the smart contract only if the boolean value is set to true
 # The function must be public so that it can be called
@@ -67,7 +76,9 @@ contract MyContract {
    }
 
 # This is just a function so that we can verify that the boolean was changed from false to true
+
    function getMyBoolean() public view  returns(bool) {
        return myBool;
    }
+
 }
